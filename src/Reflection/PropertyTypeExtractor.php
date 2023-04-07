1297fa4b2cb8c303335626eabfdd9b20f12af354
André Laugks <al@pb82.de>
2023-04-07 11:24:27 +0200

    Optimize PropertyTypeExtractor

--- src/Reflection/PropertyTypeExtractor.php
+++ src/Reflection/PropertyTypeExtractor.php
@@ -6,56 +6,37 @@ use Symfony\Component\PropertyInfo\PropertyTypeExtractorInterface;
 use Symfony\Component\PropertyInfo\Type;
 
 /**
- * This PropertyTypeExtractor is created from methods of the ReflectionExtractor
+ * This PropertyTypeExtractor is based on methods and logics of the ReflectionExtractor.
  *
  * @see \Symfony\Component\PropertyInfo\Extractor\ReflectionExtractor
  */
 class PropertyTypeExtractor implements PropertyTypeExtractorInterface
 {
-    /**
-     * @see \Symfony\Component\PropertyInfo\Extractor\ReflectionExtractor::MAP_TYPES
-     */
     private const MAP_TYPES = [
         'integer' => Type::BUILTIN_TYPE_INT,
         'boolean' => Type::BUILTIN_TYPE_BOOL,
         'double' => Type::BUILTIN_TYPE_FLOAT,
     ];
 
-    /**
-     * @see \Symfony\Component\PropertyInfo\Extractor\ReflectionExtractor::extractFromPropertyDeclaration()
-     */
     public function getTypes(string $class, string $property, array $context = []): ?array
     {
         try {
             $reflectionClass = new \ReflectionClass($class);
 
-            if (\PHP_VERSION_ID >= 70400) {
-                $reflectionProperty = $reflectionClass->getProperty($property);
-                $reflectionPropertyType = $reflectionProperty->getType();
+            $reflectionProperty = $reflectionClass->getProperty($property);
+            $reflectionPropertyType = $reflectionProperty->getType();
 
-                if (null !== $reflectionPropertyType && $types = $this->extractFromReflectionType($reflectionPropertyType, $reflectionProperty->getDeclaringClass())) {
-                    return $types;
-                }
+            if (null !== $reflectionPropertyType && $types = $this->extractFromReflectionType($reflectionPropertyType, $reflectionProperty->getDeclaringClass())) {
+                return $types;
             }
+
         } catch (\ReflectionException $e) {
-            return null;
+            // Do nothing
         }
 
-        $defaultValue = $reflectionClass->getDefaultProperties()[$property] ?? null;
-
-        if (null === $defaultValue) {
-            return null;
-        }
-
-        $type = \gettype($defaultValue);
-        $type = static::MAP_TYPES[$type] ?? $type;
-
-        return [new Type($type, $this->isNullableProperty($class, $property), null, Type::BUILTIN_TYPE_ARRAY === $type)];
+        return null;
     }
 
-    /**
-     * @see \Symfony\Component\PropertyInfo\Extractor\ReflectionExtractor::extractFromReflectionType()
-     */
     private function extractFromReflectionType(\ReflectionType $reflectionType, \ReflectionClass $declaringClass): array
     {
         $types = [];
@@ -68,60 +49,22 @@ class PropertyTypeExtractor implements PropertyTypeExtractorInterface
             }
 
             $phpTypeOrClass = $type->getName();
-            if ('null' === $phpTypeOrClass || 'mixed' === $phpTypeOrClass || 'never' === $phpTypeOrClass) {
+
+            if ($phpTypeOrClass === 'mixed') {
                 continue;
             }
 
+            $phpTypeOrClass = static::MAP_TYPES[$phpTypeOrClass] ?? $phpTypeOrClass;
+
             if (Type::BUILTIN_TYPE_ARRAY === $phpTypeOrClass) {
                 $types[] = new Type(Type::BUILTIN_TYPE_ARRAY, $nullable, null, true);
-            } elseif ('void' === $phpTypeOrClass) {
-                $types[] = new Type(Type::BUILTIN_TYPE_NULL, $nullable);
             } elseif ($type->isBuiltin()) {
                 $types[] = new Type($phpTypeOrClass, $nullable);
             } else {
-                $types[] = new Type(Type::BUILTIN_TYPE_OBJECT, $nullable, $this->resolveTypeName($phpTypeOrClass, $declaringClass));
+                $types[] = new Type(Type::BUILTIN_TYPE_OBJECT, $nullable, $phpTypeOrClass);
             }
         }
 
         return $types;
-    }
-
-    /**
-     * @see \Symfony\Component\PropertyInfo\Extractor\ReflectionExtractor::resolveTypeName()
-     */
-    private function resolveTypeName(string $name, \ReflectionClass $declaringClass): string
-    {
-        if ('self' === $lcName = strtolower($name)) {
-            return $declaringClass->name;
-        }
-        if ('parent' === $lcName && $parent = $declaringClass->getParentClass()) {
-            return $parent->name;
-        }
-
-        return $name;
-    }
-
-    /**
-     * Taken from ReflectionExtractor::isNullableProperty()
-     *
-     * @see \Symfony\Component\PropertyInfo\Extractor\ReflectionExtractor::isNullableProperty()
-     */
-    private function isNullableProperty(string $class, string $property): bool
-    {
-        try {
-            $reflectionProperty = new \ReflectionProperty($class, $property);
-
-            if (\PHP_VERSION_ID >= 70400) {
-                $reflectionPropertyType = $reflectionProperty->getType();
-
-                return null !== $reflectionPropertyType && $reflectionPropertyType->allowsNull();
-            }
-
-            return false;
-        } catch (\ReflectionException $e) {
-            // Return false if the property doesn't exist
-        }
-
-        return false;
     }
 }
---
+++ tests/Functional/PropertyTypeExtractorTest.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace Functional;
+
+use App\Normalizer\Value\StringValue;
+use App\Normalizer\Value\ValueInterface;
+use App\Reflection\PropertyTypeExtractor;
+use PHPUnit\Framework\TestCase;
+
+class PropertyTypeExtractorTest extends TestCase
+{
+    private PropertyTypeExtractor $propertyTypeExtractor;
+
+    public function setUp(): void
+    {
+        $this->propertyTypeExtractor = new PropertyTypeExtractor();
+    }
+
+    public function testPropertyA()
+    {
+        $types = $this->propertyTypeExtractor->getTypes(DummyClass::class, 'propertyA');
+
+        $this->assertSame('string', $types[0]->getBuiltinType());
+        $this->assertTrue($types[0]->isNullable());
+    }
+
+    public function testPropertyB()
+    {
+        $types = $this->propertyTypeExtractor->getTypes(DummyClass::class, 'propertyB');
+
+        $this->assertSame('object', $types[0]->getBuiltinType());
+        $this->assertSame(StringValue::class, $types[0]->getClassName());
+        $this->assertTrue($types[0]->isNullable());
+    }
+
+    public function testPropertyC()
+    {
+        $types = $this->propertyTypeExtractor->getTypes(DummyClass::class, 'propertyC');
+
+        $this->assertSame('object', $types[0]->getBuiltinType());
+        $this->assertSame(ValueInterface::class, $types[0]->getClassName());
+        $this->assertFalse($types[0]->isNullable());
+
+        $this->assertSame('string', $types[1]->getBuiltinType());
+        $this->assertFalse($types[1]->isNullable());
+
+        $this->assertSame('int', $types[2]->getBuiltinType());
+        $this->assertFalse($types[2]->isNullable());
+    }
+
+    public function testPropertyD()
+    {
+        $types = $this->propertyTypeExtractor->getTypes(DummyClass::class, 'propertyD');
+
+        $this->assertSame('string', $types[0]->getBuiltinType());
+        $this->assertTrue($types[0]->isNullable());
+    }
+
+    public function testPropertyE()
+    {
+        $types = $this->propertyTypeExtractor->getTypes(DummyClass::class, 'propertyE');
+
+        $this->assertNull($types);
+    }
+}
+
+class DummyClass extends DummyClassExtended
+{
+    private ?string $propertyA;
+
+    private ?StringValue $propertyB;
+
+    private string|int|ValueInterface $propertyC;
+}
+
+class DummyClassExtended
+{
+    protected ?string $propertyD;
+
+    protected mixed $propertyE;
+}
